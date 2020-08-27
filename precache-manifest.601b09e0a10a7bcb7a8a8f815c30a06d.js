2a5f87f5f1cc2a2abd1367b0a88fcb8ffb5e5aeb
Kebtarum <mura_aktau@mail.ru>
2020-08-27 07:01:44 +0300

    Updates

---
+++ precache-manifest.601b09e0a10a7bcb7a8a8f815c30a06d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f90ad53c1be315b60dab4291939008",
+    "url": "/ta-emphasoft/index.html"
+  },
+  {
+    "revision": "bb12f88fc561e9e65b7f",
+    "url": "/ta-emphasoft/static/css/main.3ec9a589.chunk.css"
+  },
+  {
+    "revision": "7b2d9553e9c8dae65f56",
+    "url": "/ta-emphasoft/static/js/2.4ec24491.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ta-emphasoft/static/js/2.4ec24491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb12f88fc561e9e65b7f",
+    "url": "/ta-emphasoft/static/js/main.eb99549b.chunk.js"
+  },
+  {
+    "revision": "52b0f506d62e85d2eeb0",
+    "url": "/ta-emphasoft/static/js/runtime-main.428f111f.js"
+  }
+]);
